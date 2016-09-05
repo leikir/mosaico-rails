eebf9907e16478bd98cf7c6a4c4f22e12b13fa54
yyaegash <yoko.yaeg@gmail.com>
2016-09-05 11:01:25 +0200

    Add edres path

--- app/controllers/rails_mosaico/email_templates_controller.rb
+++ app/controllers/rails_mosaico/email_templates_controller.rb
@@ -5,6 +5,6 @@ class RailsMosaico::EmailTemplatesController < ActionController::Base
   def versafix
   	original_template = Rails.application.assets.find_asset('rails_mosaico/versafix-1/template-versafix-1.html').to_s
   	new_template = ERB.new original_template.gsub(/<img src="img\/social_def\/(.*\.png|jpg|jpeg|tiff)*.+\/>/, '<%=  ActionController::Base.helpers.image_tag("rails_mosaico/versafix/img/social_def/\1", class:"socialIcon") %>')
-  	render html: new_template.result.html_safe
+    render html: new_template.result.html_safe
   end
 end
