b234a424bc9018ad883de13bb1c2aeb2abb7a3f8
yyaegash <yoko.yaeg@gmail.com>
2016-09-05 16:37:45 +0200

    Add config variable: auto_load

--- lib/rails_mosaico.rb
+++ lib/rails_mosaico.rb
@@ -2,5 +2,8 @@ require "rails_mosaico/engine"
 require "tinymce-rails"
 
 module RailsMosaico
-  # Your code goes here...
+  mattr_accessor :owner_class
+  mattr_accessor :auto_init
+
+  self.auto_init = true
 end
