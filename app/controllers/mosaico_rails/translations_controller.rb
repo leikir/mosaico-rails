bcfa78f4157a9556b908f0fe058cda1e88715be6
Hugo FOYART <hugo.foyart@epitech.eu>
2016-09-09 11:04:27 +0200

    Hotfix syntax error for translations

--- app/controllers/mosaico_rails/translations_controller.rb
+++ app/controllers/mosaico_rails/translations_controller.rb
@@ -3,7 +3,7 @@ class MosaicoRails::TranslationsController < ApplicationController
   def show
     locale = params[:locale] || 'en'
     translate_file = MosaicoRails::Engine.root.join("vendor/assets/mosaico/mosaico/dist/lang/mosaico-#{locale}.json")
-    translations = File.exists? translate_file ? translate_file : 'vendor/assets/mosaico/mosaico/dist/lang/mosaico-en.json'
+    translations = File.exists?(translate_file) ? translate_file : 'vendor/assets/mosaico/mosaico/dist/lang/mosaico-en.json'
     render :file => translations
   end
 
