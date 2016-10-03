a157b1abb98feec565a960294098a6a3b351613d
Yann Hourdel <yann@hourdel.fr>
2016-10-03 19:23:21 +0200

    Add template translations

--- app/controllers/mosaico_rails/translations_controller.rb
+++ app/controllers/mosaico_rails/translations_controller.rb
@@ -2,9 +2,21 @@ class MosaicoRails::TranslationsController < ApplicationController
 
   def show
     locale = params[:locale] || 'en'
+
     translate_file = MosaicoRails::Engine.root.join("vendor/assets/mosaico/mosaico/dist/lang/mosaico-#{locale}.json")
-    translations = File.exists?(translate_file) ? translate_file : 'vendor/assets/mosaico/mosaico/dist/lang/mosaico-en.json'
-    render :file => translations
+
+    translations = if File.exists?(translate_file)
+      JSON.parse(File.read(translate_file))
+    else
+      JSON.parse(File.read(MosaicoRails::Engine.root.join("vendor/assets/mosaico/mosaico/dist/lang/mosaico-en.json")))
+    end
+
+    template_translations_file = MosaicoRails::Engine.root.join("lib/mosaico/templates/versafix-1/versafix-1-#{locale}.json")
+    if File.exists?(template_translations_file)
+      translations.merge!({"template" => JSON.parse(File.read(template_translations_file))})
+    end
+
+    render text: translations.to_json
   end
 
 end
